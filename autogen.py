c29c6115a92c15c798302e5671368fe904c8d852
邢燕霞 <xingyanxia@bit.edu.cn>
2020-07-05 08:23:49 +0000

    丰富文件链接

--- autogen.py
+++ autogen.py
@@ -10,22 +10,28 @@ path_block = ['.git']
 suffix_block = ['md', 'py']
 suffix_img = ['jpg', 'jpeg', 'png', 'bmp']
 
+webpage_root = 'https://flyerwg.github.io/bit_move_dorm/'
+github_root = 'https://github.com/flyerwg/bit_move_dorm/tree/master/'
+
 def gen_index(path):
     'Generate index file for the dir *path*'
     filename = os.path.join(path,'README.md')
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(f'## {path}\n\n')
-        targets = sorted(os.listdir(path))
-        for target in targets:
+        targets = []
+        for target in sorted(os.listdir(path)):
             name, suffix = target.rsplit('.', 1)
             suffix = suffix.lower()
             if suffix in suffix_block: continue
+            targets.append((target, name, suffix))
+        target_count = len(targets)
+        webpage_path = webpage_root + path + '/'
+        github_path = github_root + path + '/'
+        f.write(f'共{target_count}个文件，[在网页中查看]({webpage_path})，[在Github中查看]({github_path})\n\n')
+        for target, name, suffix in targets:
             f.write(f'* [{name}](#{name})\n')
         f.write('\n')
-        for target in targets:
-            name, suffix = target.rsplit('.', 1)
-            suffix = suffix.lower()
-            if suffix in suffix_block: continue
+        for target, name, suffix in targets:
             f.write(f'### {name}\n\n')
             if suffix in suffix_img:
                 f.write(f'![{name}]({target})\n\n')
