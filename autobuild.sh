38279bce0be465a6e65e01dc4e965bf5b05d9c8f
邢燕霞 <xingyanxia@bit.edu.cn>
2020-07-06 08:52:45 +0000

    匹配版本文件名

--- autobuild.sh
+++ autobuild.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "input version string: (e.g. 1_0_2)"
+read verstr
+
 echo cleaning old files
 rm -rf build
 
@@ -8,17 +11,17 @@ chmod +x autobuild.py
 ./autobuild.py
 echo packaging all pages
 cd build
-zip -r full.zip bit_move_dorm
+zip -r bit_move_dorm_${verstr}_full.zip bit_move_dorm
 cd ..
 
 echo packaging size reduced pages
 ./autobuild.py size
 cd build
-zip -r size_reduced.zip bit_move_dorm
+zip -r bit_move_dorm_${verstr}_size_reduced.zip bit_move_dorm
 cd ..
 
 echo packaging image only pages
 ./autobuild.py img
 cd build
-zip -r image_only.zip bit_move_dorm
+zip -r bit_move_dorm_${verstr}_image_only.zip bit_move_dorm
 cd ..
